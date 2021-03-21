c33fba10a9acad6b48a6ad3d9356c61f508c19c5
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-03-21 20:18:27 +0330

    a

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
     slider_div.forEach(function (thisSlider) {
         let imgWrapper_div = thisSlider.querySelector(".slider__imgWrapper");
         let slide_div = thisSlider.querySelectorAll(".slider__slide");
-
+        slide_div[0].classList.add("slider__slide--show");
         let arrows_div = document.createElement("div");
         arrows_div.classList.add("slider__arrows");
         thisSlider.appendChild(arrows_div);
@@ -15,24 +15,63 @@ document.addEventListener('DOMContentLoaded', (event) => {
         rightArrow_img.classList.add("slider__rightArrow");
         rightArrow_img.src = "assets/rightArrow.png";
         arrows_div.appendChild(rightArrow_img);
+        
+        let index_div = document.createElement("div");
+        index_div.classList.add("slider__index");
+        thisSlider.appendChild(index_div);
+        for (let j = 0; j < slide_div.length; j++){
+            let line_div = document.createElement("div");
+            line_div.classList.add("slider__line");
+            index_div.appendChild(line_div);
+        }
+        let line_div = index_div.querySelectorAll(".slider__line");
+        line_div[0].classList.add("slider__line--active");
 
-        var currentSlide = 1;
         //console.log(img_img);
         rightArrow_img.addEventListener("click",function(){
-            for(var i =0; )
-            slide_div.forEach(function(thisSlide , index){
-                //console.log(thisSlide, index);
-                if(thisSlide.classList.contains("slider__slide--show")){
-                    thisSlide.classList.remove("slider__slide--show");
-                    currentSlide = index + 1;
-                    console.log (currentSlide);
-                    thisSlide.nextElementSibling.classList.add("slider__slide--show");
+            for (let i = 0; i < slide_div.length; i++){
+                if(slide_div[i].classList.contains("slider__slide--show")){
+                    slide_div[i].classList.remove("slider__slide--show");
+                    line_div[i].classList.remove("slider__line--active");
+                    slide_div[i].setAttribute("class" , "slider__slide ");
+                    slide_div[i].classList.add("rtl--out");
+                    if(i > slide_div.length-2){
+                        i=0;
+                        line_div[i].classList.add("slider__line--active");
+                        slide_div[i].setAttribute("class", "slider__slide slider__slide--show")
+                        slide_div[i].classList.add("rtl--in");
+                    }else{
+                        line_div[i+1].classList.add("slider__line--active");
+                        slide_div[i+1].setAttribute("class", "slider__slide slider__slide--show")
+                        slide_div[i+1].classList.add("rtl--in");
+                        break;
+                    } 
                 }
-                
-            });
+            }
         });
         leftArrow_img.addEventListener("click",function(){
-            console.log(slide_div);
+            for (let i = 0; i < slide_div.length; i++){
+                if(slide_div[i].classList.contains("slider__slide--show")){
+                    slide_div[i].classList.remove("slider__slide--show");
+                    line_div[i].classList.remove("slider__line--active");
+                    slide_div[i].setAttribute("class" , "slider__slide ");
+                    slide_div[i].classList.add("ltr--out");
+                    if(i < slide_div.length-2){
+                        i=slide_div.length-1;
+                        console.log(slide_div.length-1)
+                        slide_div[slide_div.length-1].classList.add("slider__slide--show");
+                        line_div[slide_div.length-1].classList.add("slider__line--active");
+                        slide_div[i].setAttribute("class", "slider__slide slider__slide--show")
+                        slide_div[i].classList.add("ltr--in");
+                    }else{
+                        slide_div[i-1].classList.add("slider__slide--show");
+                        line_div[i-1].classList.add("slider__line--active");
+                        slide_div[i-1].setAttribute("class", "slider__slide slider__slide--show")
+                        slide_div[i-1].classList.add("ltr--in");
+                        break;
+                    }
+                }
+            }
         });
     });
-})
+});
